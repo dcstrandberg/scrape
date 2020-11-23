c45f0e491dcb27c3eb2487d483e7d1e96ee093f1
dcstrandberg@gmail.com <dcstrandberg@gmail.com>
2020-11-23 13:24:05 -0600

    Now successfully pulls all cards from carousels, and normal search results alike. Added basic captcha checking -- if there's no data on a page, it retries

--- amazon_scrape.py
+++ amazon_scrape.py
@@ -1,14 +1,14 @@
 #import time
 #TODO: Add some form of analysis?, 
-#Add whether an item is sponsored/featured -- Amazon's Choice, maybe others?
+#Add best seller flag?
+#Add check on whether there was ANY data pulled or if it appears to be a captcha page
+    #It should try a new proxy, and if it tries... 100 proxies or something, then it should error and send a message to an email that there's an issue
 #Test out using sentiment analysis to tag the data -- brand name / pack size / pack count / $ per oz
 #Add more sources: instacart, walmart, etc. Then create wrapper program that calls each of these subroutines
 
-#Added regular price, on sale boolean, Amazon Choice Boolean and item position on the page
-#Added more robust proxy cycling -- remove bad proxies from list, and re-scrape if we get too small a list
-
 from proxy_list_scrape import scrapeProxyList
 from datetime import date
+import re
 import random
 import requests
 from bs4 import BeautifulSoup
@@ -32,12 +32,21 @@ prices=[] #List to store price of the product
 regularPrices=[] #List to store regular prices, if any
 onSales=[] #List to store boolean of whether item is on sale or not
 amazonChoices=[] #List to store boolean of whether item is an Amazon Choice product or not
+sponsoredList=[] #List to store boolean of whether item is Sponsored
 positions=[] #List of where the product was positioned in the search
 pages=[] #List to store ratings of the product
 
 #Declare the request variables that determine how many requests will get made -- eventually these will be fed as arguments to the request function from a wrapper function
-no_pages = 4 #20
-keywords = ['soda','cereal', 'bars', 'drinks', 'water', 'coke']
+no_pages = 2#4
+#keywords = ['Soda', 'Pop', 'Water', 'Sports Drinks', 'Coffee', 'Cereal', 'Snack Bars', 'Chips', 'Snacks', 'Contact Lenses', 'Coke', 'Fanta', 
+#   'Sprite', 'Powerade', 'Frosted Flakes', 'Special K', 'Froot Loops', 'Raisin Bran', 'Pringles', 'Cheez It', 'Rice Krispies', 'Rice Krispies Treats', 
+#   'Pop Tarts', 'Acuvue', 'Oasys', 'Pet Food', 'Dog Food', 'Cat Food']
+keywords = ['cereal']
+
+#Re organizing things
+# - putting proxy cycling into its own function
+# - putting request in its own function, then the different soups search into their own function?
+# - putting search term & page number into one list that can be cycled through so all the things can be threaded separately
 
 
 def get_data(keyword, pageNo,q):  
@@ -61,69 +70,125 @@ def get_data(keyword, pageNo,q):
             del proxies[proxyCounter % len(proxies)]
 
             #check to make sure we still have a sizeable list of proxies, and if it gets below 10 proxies, scrape a new list
-            if len(proxies) < 10: proxies = scrapeProxyList()
+            if len(proxies) < 5: proxies = scrapeProxyList()
 
             print("There was a connection error")
             proxyCounter += 1
         else:
             print("Now things are ok")
-            noError = True
+            #THIS IS ALL DEBUGGING NONSENSE
+            if 'http' in proxies[proxyCounter % len(proxies)]: 
+                printProxy = proxies[proxyCounter % len(proxies)]['http']
+            else:
+                printProxy = proxies[proxyCounter % len(proxies)]['https']
 
-    content = r.content
-    soup = BeautifulSoup(content, features="lxml")
+            print("This proxy worked " + printProxy)
+
+            #Add here some checking for whether soup is readable
+
+            content = r.content
+            soup = BeautifulSoup(content, features="lxml")
+            
+            if len(soup.findAll('div', attrs={'data-index':re.compile(r'\d+')})) == 0:
+                #remove the bad proxy from the list so we don't try it again
+                del proxies[proxyCounter % len(proxies)]
+
+                #check to make sure we still have a sizeable list of proxies, and if it gets below 10 proxies, scrape a new list
+                if len(proxies) < 5: proxies = scrapeProxyList()
+
+                print("There was a captcha error")
+                proxyCounter += 1
+            else:
+                noError = True
+
+    #content = r.content
+    #soup = BeautifulSoup(content, features="lxml")
     #print(soup.encode('utf-8')) # uncomment this in case there is some non UTF-8 character in the content and
                                  # you get error
 	
-    positionCounter = 0
 
-    for d in soup.findAll('div', attrs={'class':'sg-col-4-of-24 sg-col-4-of-12 sg-col-4-of-36 s-result-item s-asin sg-col-4-of-28 sg-col-4-of-16 sg-col sg-col-4-of-20 sg-col-4-of-32'}):
-        name = d.find('span', attrs={'class':'a-size-base-plus a-color-base a-text-normal'})
-        price = d.find('span', attrs={'class':'a-offscreen'})
-        
-        #I may not be able to call contents immediately within the declaration
-        regularPrice = d.find('span', attrs={'data-a-strike':'true'})
+    for d in soup.findAll('div', attrs={'data-index':re.compile(r'\d+')}):
 
-        amazonChoice = d.find('span', attrs={'class':'a-badge'})
+        #Some of those search results may be carousels, so we need to iterate for each "entry" within the search result
+        #Set up a carousel counter to add to placement entries for carousel entries
+        carouselCounter = 0      
 
-        all=[]
-		
-        all.append(keyword)
-        all.append(date.today())
-
-        if name is not None:
-            all.append(name.text)
+        if len(d.findAll('div', attrs={'data-asin':re.compile(r'.*')})) > 0:
+            soupList = d.findAll('div', attrs={'data-asin':re.compile(r'.*')})
         else:
-            all.append("unknown-product")
- 
-        if price is not None:
-            all.append(price.text)
-        else:
-            all.append('$0')
+            soupList = d.findAll('div', attrs={'class':'sg-col-inner'})
 
-        if regularPrice is not None:
-            all.append(regularPrice.contents[0].text)
-            all.append(True)
-        else:
-            all.append('$0')
-            all.append(False)
-		
-        if amazonChoice is not None:
-            all.append(True)
-        else:
-            all.append(False)
+        #Now I'm only getting the top carousel data
+        for product in soupList:
+            #print("We're in the second soup loop")
+            #if we're in the first carousel
+            if product.find('span', attrs={'data-click-el':'title'}) is not None:
+                name = product.find('span', attrs={'data-click-el':'title'})
+                ad = True
+            else:
+                name = product.find('span', attrs={'class':'a-size-base-plus a-color-base a-text-normal'})
+                ad = False
+            
+            #print(name)
+            price = product.find('span', attrs={'class':'a-offscreen'})
+            #print(price)
+            regularPrice = product.find('span', attrs={'data-a-strike':'true'})
 
-        all.append(str(positionCounter))
-        #Now increment positionCounter so the next appended value is given the correct position
-        positionCounter += 1
+            #This should work, because it's looking 
+            amazonChoice = d.find('span', attrs={'class':'a-badge'})
+            placement = d['data-index']
 
-        all.append(str(pageNo))
+            all=[]
 
-        #if rating is not None:
-            #all.append(rating.text)
-        #else:
-            #all.append('-1')
-        q.put(all)
-        #print("---------------------------------------------------------------") 
+            #Product Name and Price should be the two minimum checks before putting ANYTHING into the queue
+            #print("Checking Name: ", name)
+            if name is not None: #and price is not None:
+                
+                all.append(keyword)
+                all.append(date.today())
+                
+                all.append(name.text)
+
+                if price is not None:
+                    all.append(price.text)
+                else:
+                    all.append("$0")
+                
+                if regularPrice is not None:
+                    all.append(regularPrice.contents[0].text)
+                    all.append(True)
+                else:
+                    all.append('$0')
+                    all.append(False)
+                
+                if amazonChoice is not None:
+                    all.append(True)
+                else:
+                    all.append(False)
+
+                #The second part of the check is to look if something is within a carousel, which I'm considering a sponsored product....
+                if 'AdHolder' in d['class'] or 's-widget' in d['class'] or ad == True:
+                    all.append(True)
+                else:
+                    all.append(False)
+
+                #Using string slice to only input the # part of the string and then appending "-#" if it's one of the carousel entries
+                all.append( placement + "-" + str(carouselCounter))
+                
+                all.append(str(pageNo))
+
+            
+                q.put(all)
+
+                #increment carousel counter -- shouldn't matter if this isn't a carousel
+                carouselCounter += 1
+             
+    #DEBUGGING -- if this is page 1 of the first loop, just save the full html file
+    if pageNo == 1 and keyword == 'cereal':
+        with open('cereal_html.html', 'w', encoding='utf-8') as outfile:
+            outfile.write(str(soup))
+
+
 results = []
 if __name__ == "__main__":
     m = Manager()
@@ -135,31 +200,22 @@ if __name__ == "__main__":
     #Adding a for loop to cycle through the keywords -- not sure if a queue can work inside a for loop
     for word in keywords:
         p = {}    
-        if sys.argv[1] in ['t', 'p']: # user decides which method to invoke: thread, process or pool
-            for i in range(1,no_pages):
-                if sys.argv[1] in ['t']:
-                    print("starting thread: ",proxyCounter)
-                    p[i] = threading.Thread(target=get_data, args=(word, i,q))
-                    p[i].start()
 
-                    #increment ProxyCounter
-                    proxyCounter += 1
+        for i in range(1,no_pages):            
+            print("starting process: ",proxyCounter)
+            p[i] = Process(target=get_data, args=(word, i, q))
+            p[i].start()
 
-                elif sys.argv[1] in ['p']:
-                    print("starting process: ",proxyCounter)
-                    p[i] = Process(target=get_data, args=(word, i,q))
-                    p[i].start()
-
-                    #increment ProxyCounter
-                    proxyCounter += 1
+            #increment ProxyCounter
+            proxyCounter += 1
 
             # join should be done in seperate for loop 
             # reason being that once we join within previous for loop, join for p1 will start working
             # and hence will not allow the code to run after one iteration till that join is complete, ie.
             # the thread which is started as p1 is completed, so it essentially becomes a serial work instead of 
             # parallel
-            for i in range(1,no_pages):
-                p[i].join()
+        for i in range(1,no_pages):
+            p[i].join()
         while q.empty() is not True:
             qcount = qcount+1
             queue_top = q.get()
@@ -170,13 +226,14 @@ if __name__ == "__main__":
             regularPrices.append(queue_top[4])
             onSales.append(queue_top[5])
             amazonChoices.append(queue_top[6])
-            positions.append(queue_top[7])
-            pages.append(queue_top[8])
+            sponsoredList.append(queue_top[7])
+            positions.append(queue_top[8])
+            pages.append(queue_top[9])
 
     #Only run once everything is done        
     print("total time taken: ", str(time.time()-startTime), " qcount: ", qcount)
     #print(q.get())
     df = pd.DataFrame({'Keyword':searchTerms,'Date':dates, 'Product Name':products, 'Price':prices, 
-        'Regular Price:':regularPrices, 'On Sale?':onSales, 'Amazon Choice':amazonChoices, 'List Position':positions, 'Page':pages})
+        'Regular Price:':regularPrices, 'On Sale?':onSales, 'Amazon Choice':amazonChoices, 'Sponsored':sponsoredList, 'List Position':positions, 'Page':pages})
     #print(df)
     df.to_csv('./amazon_data/' + str(date.today()) + '-SearchList.csv', index=False, encoding='utf-8')
